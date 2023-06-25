38d0f52da968979757cfa981778d5fe4f2a2864e
kgolden4514 <kmadams6@ncsu.edu>
2023-06-25 16:13:23 -0400

    y

--- render.R
+++ render.R
@@ -3,3 +3,5 @@ library(rmarkdown)
 rmarkdown::render("C:/Users/kmada/OneDrive - Onslow County Schools/3. ST 558/6a. Projects/Project1/Project_1.Rmd", output_format = 'github_document',
        output_file = 'README.md', output_dir = 'C:/Users/kmada/OneDrive - Onslow County Schools/3. ST 558/6a. Projects/Project1/_includes')
 
+rmarkdown::render("C:/Users/kmada/OneDrive - Onslow County Schools/3. ST 558/6a. Projects/Project1/Project_1.Rmd", output_format = 'github_document',
+                  output_file = '2023-06-25-Finance-API-Vignette.html', output_dir = 'C:/Users/kmada/OneDrive - Onslow County Schools/3. ST 558/6a. Projects/Project1/_posts')
