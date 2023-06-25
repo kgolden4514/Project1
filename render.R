58901cadb179703e6280ecad302c08da4c07761e
kgolden4514 <kmadams6@ncsu.edu>
2023-06-25 14:14:29 -0400

    created render file

---
+++ render.R
@@ -0,0 +1,5 @@
+
+library(rmarkdown)
+rmarkdown::render("C:/Users/kmada/OneDrive - Onslow County Schools/3. ST 558/6a. Projects/Project1/Project_1.Rmd", output_format = 'github_document',
+       output_file = 'README.md', output_dir = 'C:/Users/kmada/OneDrive - Onslow County Schools/3. ST 558/6a. Projects/Project1/_includes')
+
